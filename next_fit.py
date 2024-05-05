0eb37b699724b77a2290d8e3a503971a92907645
Jonathan Tran <tranjt7@uci.edu>
2024-05-04 20:11:23 -0700

    added main function to next fit

--- next_fit.py
+++ next_fit.py
@@ -1,6 +1,14 @@
 # Example file: next_fit.py
-
+import decimal as dec
 # explanations for member functions are provided in requirements.py
 
 def next_fit(items: list[float], assignment: list[int], free_space: list[float]):
 	pass
+
+if __name__ == '__main__':
+	a = dec.Decimal('0.1')
+	b = dec.Decimal('0.2')
+	c = dec.Decimal('0.3')
+	print('a+b = ', a + b)
+	print('c = ', c)
+	print('does a+b == c? ', a + b == c)
