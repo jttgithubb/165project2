40e83d6065e3fd5d33b88bb900c8015c5436ec57
Jonathan Tran <tranjt7@uci.edu>
2024-05-07 08:11:26 -0700

    adds more precision to next fit

--- next_fit.py
+++ next_fit.py
@@ -8,8 +8,8 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 	free_space.append(1.0)
 	for i in range (n):
 		item_f = items[i]
-		item_d = dec.Decimal('{:.2f}'.format(item_f))  # item size
-		bin_space_d = dec.Decimal('{:.2f}'.format(free_space[-1]))  # available space in last bin
+		item_d = dec.Decimal('{:.10f}'.format(item_f))  # item size
+		bin_space_d = dec.Decimal('{:.10f}'.format(free_space[-1]))  # available space in last bin
 		if item_d <= bin_space_d:  # if item fits in last bin
 			assignment[i] = len(free_space) - 1
 			# update the free space of the last bin precisely
@@ -17,6 +17,6 @@ def next_fit(items: list[float], assignment: list[int], free_space: list[float])
 		else:  # if item does not fit in last bin
 			free_space.append(1.0)
 			assignment[i] = len(free_space) - 1
-			bin_space_d = dec.Decimal('{:.2f}'.format(free_space[-1]))  # available space in last bin that was added
+			bin_space_d = dec.Decimal('{:.10f}'.format(free_space[-1]))  # available space in last bin that was added
 			free_space[-1] = float(bin_space_d - item_d)
 
