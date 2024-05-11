7ead9dde6015245f95e318463b2ce9eb2bb6230f
Jonathan Tran <tranjt7@uci.edu>
2024-05-11 02:21:12 -0700

    removed some comments

--- best_fit.py
+++ best_fit.py
@@ -9,7 +9,6 @@ def best_fit(items: list[float], assignment: list[int], free_space: list[float])
     new_bin_id = 0
     free_space.append(new_bin_cap)
     bins_tree = ZipZipTree(n)
-    #bins_tree.insert(new_bin_cap, new_bin_id)  # Initial bin node [Node 0, key:1.0, val:0, bVal:0...]
     initial_key = (dec.Decimal('{:.10f}'.format(new_bin_cap)), new_bin_id)  # Handles duplicate capacities
     initial_bin = bins_tree.insert(initial_key, new_bin_id)  # Initial bin node [Node 0, key:(Decimal capacity, Integer bin_id), val:0, bVal:0 ... AND cap:1.0] 
     initial_bin.cap = new_bin_cap
@@ -17,8 +16,6 @@ def best_fit(items: list[float], assignment: list[int], free_space: list[float])
     for i in range(n):
         item_f = items[i]
         item_d = dec.Decimal('{:.10f}'.format(item_f))  # get item size
-        # function to find the node that will BEST hold the item or None then insert a new node to hold the item
-        #bin_found = bins_tree.find_bin2(item_d)
         bin_found = bins_tree.find_bin3(item_d)
         if bin_found is not None:  # place item in bin and update the tree
             bin_key = bin_found.key
